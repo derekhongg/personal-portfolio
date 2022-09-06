45228c5fadab5fd4908e9bdffc4303e47a1f0b22
derekhongg <hongderek@gmail.com>
2022-09-05 17:08:59 -0700

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.scss';
+import { Routes, Route } from 'react-router-dom';
+import Layout from './components/Layout';
+
+function App() {
+  return (
+    <div className="App">
+      <>
+      <Routes>
+        <Route path="/" element={<Layout/>}/>
+      </Routes>
+      </>
+    </div>
+  );
+}
+
+export default App;
